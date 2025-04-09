391ad9ca130219f738e1ca374f767bdd91586893
SBonard <152468400+SBonard@users.noreply.github.com>
2025-04-09 18:13:14 +0100

    18:13

---
+++ healthbridge/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\APPS\\flutter\\flutter_windows_3.29.0-stable" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\SB\\Documents\\GitHub\\telemedecine\\healthbridge" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\APPS\\flutter\\flutter_windows_3.29.0-stable"
+  "PROJECT_DIR=C:\\Users\\SB\\Documents\\GitHub\\telemedecine\\healthbridge"
+  "FLUTTER_ROOT=C:\\APPS\\flutter\\flutter_windows_3.29.0-stable"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\SB\\Documents\\GitHub\\telemedecine\\healthbridge\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\SB\\Documents\\GitHub\\telemedecine\\healthbridge"
+  "FLUTTER_TARGET=C:\\Users\\SB\\Documents\\GitHub\\telemedecine\\healthbridge\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\SB\\Documents\\GitHub\\telemedecine\\healthbridge\\.dart_tool\\package_config.json"
+)
